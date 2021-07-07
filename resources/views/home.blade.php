af3e5b522db5f293a8e4795b3441f833516fee3d
並木睦稀 <mutsuki@namikimutsumimarenoMacBook-Air.local>
2021-07-07 17:03:38 +0900

    テスト更新
    テスト

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<?php 
+echo 'test';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Hello,World</h1>
+    <button class ="btn">aaa</button>
+    <a href="">1111</a>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
